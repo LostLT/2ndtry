232cdaf3ace992a6f760e6974ebf546627290e6d
LostLT <Adam@Administrators-iMac.local>
2014-08-20 16:52:36 -0400

    firstcommit
    firstcommit

---
+++ webcamforealz/Assets/WebCameraScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WebCameraScript : MonoBehaviour 
+{
+	public GameObject myCameraTexture;
+	private WebCamTexture webCameraTexture;
+	void Start () 
+	{
+		for (int cameraIndex = 0; cameraIndex < WebCamTexture.devices.Length; cameraIndex++) 
+		{
+			Debug.Log ("Cam:"+WebCamTexture.devices[cameraIndex].name+WebCamTexture.devices[cameraIndex].isFrontFacing);
+			if(WebCamTexture.devices[cameraIndex].isFrontFacing)
+			{
+				Debug.Log ("Selected cam:"+WebCamTexture.devices[cameraIndex].name+WebCamTexture.devices[cameraIndex].isFrontFacing);
+				webCameraTexture = new WebCamTexture (WebCamTexture.devices[cameraIndex].name);
+				break;
+			}
+		}    
+		myCameraTexture.renderer.material.mainTexture = webCameraTexture;
+		webCameraTexture.Play ();
+		Debug.Log (webCameraTexture.videoVerticallyMirrored);
+
+	}
+}
